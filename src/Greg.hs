ca12709a61ef9a3983921222b9d8c5d032ed4c70
Mike Ledger <eleventynine@gmail.com>
2012-04-11 21:09:08 +1000

    sorry Greg.hs

---
+++ src/Greg.hs
@@ -0,0 +1,28 @@
+module Greg where
+
+import Greg.Simple
+import Greg.IRC     (connect)
+import Greg.Types   (Command, Message, Bot (..))
+
+import Data.Text (Text)
+import Control.Concurrent
+import Control.Monad
+
+-- | Simplest way to get a bot up and running.
+-- This function logs to stdout with 'Greg.Simple.monitor', and makes a small IRC client that you can type into
+-- to send messages to the channel, using 'Greg.Simple.client'
+greg :: String    -- ^ Path to config
+     -> [Command] -- ^ available commands
+     -> (Message -> Bot -> IO (Maybe Text)) -- ^ Message handler (for messages other than commands)
+     -> IO ()
+
+greg path coms msgh = do
+    bot <- do
+        tmpBot <- createBot path
+        connect tmpBot { 
+                commands   = coms,
+                msgHandler = msgh
+            }
+
+    forkIO  (client  bot)
+    forever (monitor bot)
